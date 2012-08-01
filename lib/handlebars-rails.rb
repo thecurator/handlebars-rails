81350071accf8137b9b022c63fb9e25454a8cf3b
Charles Lowell <cowboyd@thefrontside.net>
2012-08-01 23:02:16 +0300

    support for partials

--- lib/handlebars-rails.rb
+++ lib/handlebars-rails.rb
@@ -1,19 +1,51 @@
 require 'handlebars-rails/version'
 require 'handlebars'
-require "active_support"
+require 'active_support'
 
 module Handlebars
   class TemplateHandler
 
     def self.handlebars
-      @context ||= Handlebars::Context.new
+      @context ||= new_context
     end
 
     def self.call(template)
       %{
-        handlebars = ::Handlebars::TemplateHandler.handlebars
-        handlebars.compile(#{template.source.inspect}).call(assigns).force_encoding(Encoding.default_external).html_safe
+        handler = ::Handlebars::TemplateHandler
+        handlebars = handler.handlebars
+        handler.with_view(self) do
+          handlebars.compile(#{template.source.inspect}).call(assigns).force_encoding(Encoding.default_external).html_safe
+        end
       }
+    end
+
+    def self.with_view(view)
+      original_view = data['view']
+      data['view'] = view
+      yield
+    ensure
+      data['view'] = original_view
+    end
+
+    def self.new_context
+      Handlebars::Context.new.tap do |context|
+        context['rails'] = {}
+        context.partial_missing do |name|
+          lookup_context = data['view'].lookup_context
+          partial = lookup_context.find(name, lookup_context.prefixes, true)
+          if partial.handler == self
+            partial.source
+          else
+            lambda do |this, context, options|
+              data['view'].render :partial => name, :locals => context
+            end
+          end
+        end
+      end
+    end
+
+    def self.data
+      @context['rails']
     end
   end
 end
--- spec/render_spec.rb
+++ spec/render_spec.rb
@@ -3,19 +3,43 @@ require 'spec_helper'
 
 describe Handlebars::TemplateHandler do
 
-  def assigns
-    @assigns ||= {:name => 'World'}
+  before do
+    @lookup_context = mock('ActionView::LookupContext', :prefixes => [:one])
+    @assigns = {:name => 'World'}
+    @view = mock('ActionView::Base', :lookup_context => @lookup_context, :assigns => @assigns)
   end
 
   it 'should be able to render a basic HTML template' do
-    template = Template.new('basic_html', '<h1>Hello</h1>')
-    compiled_template_source = Handlebars::TemplateHandler.call(template)
-    eval(compiled_template_source).should eql '<h1>Hello</h1>'
+    render_template('basic_html', '<h1>Hello</h1>').should eql '<h1>Hello</h1>'
   end
 
   it 'renders handlebars templates' do
-    template = Template.new('hbs_html', '<h1>Hello {{name}}</h1>')
+    render_template('hbs_html', '<h1>Hello {{name}}</h1>').should eql '<h1>Hello World</h1>'
+  end
+
+  describe 'an embedded handlebars partial' do
+    before do
+      @lookup_context.stub(:find).with("hbs", [:one], true) {mock(:source => "{{name}}", :handler => Handlebars::TemplateHandler)}
+    end
+
+    it 'renders' do
+      render_template('hbs_partial', '<h1>Hello {{>hbs}}</h1>').should eql '<h1>Hello World</h1>'
+    end
+  end
+
+  describe 'an embedded erb partial' do
+    before do
+      @lookup_context.stub(:find).with("erb", [:one], true) {mock(:source => "<%= @name %>", :handler => mock(:ERB))}
+      @view.stub(:render).with(hash_including(:partial => 'erb')) {|options| options[:locals][:name]}
+    end
+    it 'renders' do
+      render_template('erb_partial', '<h1>Hello {{>erb}}</h1>').should eql '<h1>Hello World</h1>'
+    end
+  end
+
+  def render_template(name, source)
+    template = Template.new(name, source)
     compiled_template_source = Handlebars::TemplateHandler.call(template)
-    eval(compiled_template_source).should eql '<h1>Hello World</h1>'
+    @view.instance_eval(compiled_template_source)
   end
 end
